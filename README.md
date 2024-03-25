e0ca2104ef3f27eb0ef40553b275997ae320bf4c
ro-shah <shahronil1001@gmail.com>
2024-03-24 21:48:56 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SWAI
+work-in-progress product than utilizies AI to create study problems to help students study
---
+++ testingfiles/basicquestions.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+
+OPENAI_API_KEY = "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"
+MODEL = "gpt-4-turbo-preview"
+
+def getQuestionData():
+    subject_of_question = input("Subject?: ")
+    type_of_question = input("Type of question?: ")
+    
+    return type_of_question, subject_of_question
+
+def getQuestion(question_format, input_text):
+    client = OpenAI(api_key = OPENAI_API_KEY)
+    response = client.chat.completions.create(
+        model = MODEL,
+        messages = [
+            {"role": "user", "content": "Write to me a " + question_format + "question that tests my knowledge about " + input_text},
+        ],
+        temperature = 0,
+    )
+    
+    return response
+
+if __name__ == "__main__":    
+    question_format, input_text = getQuestionData()
+    completion = getQuestion(question_format, input_text)
+    print(completion.choices[0].message.content)
